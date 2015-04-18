09959122a252f3613b2fdbbb71161f13192ac1c6
DavidMonson <davidcmonson@gmail.com>
2015-04-17 19:01:33 -0600

    Finishes pulling preset exercises down from parse

---
+++ Neuroplasticity/ExercisePresetsController.h
@@ -0,0 +1,13 @@
+//
+//  ExercisePresetsDataSource.h
+//  Neuroplasticity
+//
+//  Created by David Monson on 4/16/15.
+//  Copyright (c) 2015 David Monson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ExercisePresetsDataSource : NSObject
+
+@end
